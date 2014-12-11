f91997c02e4975884cf16c8cfd9c319b1e13cfd3
MWATermin <hutzeldupf@gmx.de>
2014-12-11 20:00:54 +0100

    Initial

---
+++ src/calendar/Date.java
@@ -0,0 +1,117 @@
+package calendar;
+
+import java.io.*;
+import java.util.*;
+import javax.persistence.*;
+
+/*
+ID (Int) //autoincrement
+Datum des Termins (Calendar)
+Uhrzeit (Siehe oben)
+Dauer (Int minuten)
+Name des Erstellers (String)
+Ort (String)
+Terminname (String)
+Beschreibung (String)
+Teilnehmende Personen (List member)
+*/
+
+
+@Entity
+public class Date implements Serializable {
+	
+	// Variablendeklaration
+	private Integer id;
+	private Calendar dateAndTime;
+	private Integer duration;
+	private String author;
+	private String place;
+	private String label;
+	private String description;
+	private ArrayList<String> members;
+	
+	// Default Konstruktor
+	public Date() {
+		super();
+	}
+	
+	// Konstruktor
+	public Date( Calendar dateAndTime, Integer duration, 
+			String author, String place, String label, String description, ArrayList<String> members) {
+		super();
+		this.dateAndTime = dateAndTime;
+		this.duration = duration;
+		this.author = author;
+		this.place = place;
+		this.label = label;
+		this.description = description;
+		this.members = members;
+	}
+	
+	// Getters & Setters
+	@Id
+	@GeneratedValue // Sorgt daf�r, das ID automatisch erzeugt wird
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId( Integer id) {
+		this.id = id;
+	}
+	
+	public Calendar getDateAndTime() {
+		return dateAndTime;
+	}
+
+	public void setDateAndTime( Calendar dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration( Integer duration) {
+		this.duration = duration;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor( String author) {
+		this.author = author;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace( String place) {
+		this.place = place;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel( String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription( String description) {
+		this.description = description;
+	}
+	
+	public ArrayList<String> getMembers() {
+		return members;
+	}
+
+	public void setMembers( ArrayList<String> members) {
+		this.members = members;
+	}
+}
---
+++ src/calendar/calendarClient.java
@@ -0,0 +1,109 @@
+package calendar;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+import java.util.ListIterator;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class calendarClient {
+	
+	public static void main(String[] args) throws Exception {
+		invokeStatelessBean();
+	}
+
+	private static void invokeStatelessBean() throws NamingException {
+		final CalRemoteInterface CalendarInterface = doLoopup();
+		System.out.println("Obtained a Quoter for invocation"); 
+		
+		System.out.println("createQuote\n");
+		/* int myid = CalenderInterface.createDate("HelloDate");
+		System.out.println("CalendarID: " + myid + "\n");
+		
+		System.out.println("readQuote\n");
+		System.out.println(CalenderInterface.readText( myid));
+		
+		
+		*/
+		String username = "John";
+		ArrayList<String> members = new ArrayList<String>();
+		members.add(username);
+		
+		Calendar cal = new GregorianCalendar(2013,1,28,13,24,56);
+		
+		Date date = new Date(cal, 30, "bla", "cok", "suking", "gangban111", null);
+		//int myid = CalenderInterface.createDate();
+		Integer myid = CalendarInterface.createDate( date, username);
+		System.out.println("CalendarID1: " + myid + "\n");
+		
+		cal = new GregorianCalendar(2014,1,28,13,25,56);
+		date = new Date(cal, 30, "bla", "cok", "suking", "gangban222", null);
+		myid = CalendarInterface.createDate( date, username);
+		System.out.println("CalendarID2: " + myid + "\n");
+		
+		
+		//Update Date Test-Case
+		Date d = new Date(cal, 30, "blab", "bad", "beer", "gangban11", members);
+		CalendarInterface.updateDate(1, d);
+		System.out.println("updateDate(1, d)");
+		
+		d = new Date(cal, 30, username, "bad", "beer123", "gangban11", members);
+		CalendarInterface.updateDate(1, d);
+		System.out.println("updateDate(1, d)");
+		
+		
+		//CalendarInterface.deleteDate(myid, username);
+		
+		
+		// Print ArrayList<Date>
+		ArrayList<Date> DateList;
+		DateList = CalendarInterface.getAllDatesInDB( username);
+		System.out.println("getAllDatesInDB()");
+		
+		ListIterator<Date> li = DateList.listIterator();
+		System.out.println("size: " + DateList.size() + "\n");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		while( li.hasNext()) {
+			Date serverDate = DateList.get( li.nextIndex());
+			System.out.println( "ID: " + serverDate.getId() + "\n" + 
+ 					"Description: " + serverDate.getDescription() + "\n" +
+					"Author: " + serverDate.getAuthor() + "\n" +
+					"Label: " + serverDate.getLabel() + "\n" +
+					"Members: " + serverDate.getMembers() + "\n" +
+					"Place: " + serverDate.getPlace() + "\n" +
+					"Duration: " + serverDate.getDuration() + "\n" + 
+					"DateAndTime: " + dateFormat.format( serverDate.getDateAndTime().getTime()) + "\n"
+					);
+			
+			li.next();
+			
+		}
+		// ENDE Print ArrayList<Date>
+	}
+
+	// Looks up and returns the proxy to remote interface
+	private static CalRemoteInterface doLoopup() throws NamingException{
+		final Hashtable jndiProperties = new Hashtable();
+		jndiProperties.put(Context.URL_PKG_PREFIXES,"org.jboss.ejb.client.naming");
+		final Context context = new InitialContext(jndiProperties);
+		// The app is typically the ear name
+		final String appName = "";
+		// This is the module name of the deployed EJBs on the server
+		final String moduleName = "Calendar_Rest";
+		final String distinctName = "";
+		// The EJB name which by default is the simple class name of the bean // implementation class
+		final String beanName = Cal.class.getSimpleName();
+		// the remote view fully qualified class name
+		final String viewClassName = CalRemoteInterface.class.getName(); // let's do the lookup
+		String lookupName = "ejb:" + appName + "/" + moduleName + "/"
+				+ distinctName + "/" + beanName + "!" + viewClassName;
+		System.out.println(lookupName);
+		return (CalRemoteInterface) context.lookup(lookupName);
+	}
+}
